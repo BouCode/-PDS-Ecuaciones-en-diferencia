772c2eae1bd3fbfe98afbe2b5be110ed6d7eec7c
Joan Boulangger <joan.boulangger@gmail.com>
2020-08-11 10:09:27 -0500

    [Cuestionario]: 1, 2, 4, 10

---
+++ Cuestionario/problema1.py
@@ -0,0 +1,23 @@
+from scipy import signal
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+import math 
+
+if __name__ == '__main__':
+    n_min, n_max = 0, 70
+    n = np.arange (n_min, n_max)
+    A = np.array ([1, 0.13, 0.52, 0.30])
+    B = np.array ([0.16, -0.48, 0.48])
+    x = np.append(1, np.zeros(len(n)-1))
+    y = signal.lfilter (B, A, x)
+    fig, axs = plt.subplots (2, 1)
+    axs[0].stem (n, x)
+    axs[0].set_xlabel ('n')
+    axs[0].set_ylabel ('x[n]')
+    axs[0].set_title ('Entrada')
+    axs[1].stem (n, y)
+    axs[1].set_xlabel ('n')
+    axs[1].set_ylabel ('y[n]')
+    axs[1].set_title ('Salida')
+    plt.show()
